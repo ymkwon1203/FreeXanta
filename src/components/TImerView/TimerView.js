9081dbd928e57db50d15a7a5ca5187c6cc1df01d
sixone <sixone0712@gmail.com>
2020-03-13 15:03:25 +0900

    Timmer관련수정

--- src/components/TImerView/TimerView.js
+++ src/components/TImerView/TimerView.js
@@ -1,8 +1,9 @@
 import React, {Component, Fragment} from "react";
 import {Row, Col, Container, Spinner} from "reactstrap";
 import Timer from "react-compound-timer";
-import {FaRegCircle} from "react-icons/fa";
+import {FaCircle} from "react-icons/fa";
 import { GiCircle } from "react-icons/gi";
+import {MdRefresh} from "react-icons/md";
 
 const ContainerStyle = {
 	position: "relative",
@@ -39,27 +40,55 @@ const CirCleStyle = {
 	transform: "translate(-50%, 0%)",
 }
 
+const ResetStyle = {
+	position: "absolute",
+	top : "319%",
+	left: "64%"
+};
+
+const ResetAction = (props) => {
+
+	const { reset, getTime } = props;
+
+	console.log("getTime()", getTime());
+	if(getTime() < 0) {
+		reset();
+	}
+	return <div></div>;
+}
+
 class TimerView extends Component {
 
 	constructor() {
 		super();
-
 		this.state = {
 			timerState: "init",
-			initialTime: 60000
+			initialTime: 60000,
+			resetFunc : null,
 		};
 	};
 
-	onResetFunc = (reset) => () => {
-		console.log('My own function');
-		reset()
-	};
+	test = (reset, getTime) => {
+		console.log("getTime()", getTime());
+		if(getTime() <= 0) {
+			reset();
+		}
+
+		return null;
+	}
+
+	resetAction = (el) => {
+		el.click();
+	}
+
+
+
 	render() {
 		return (
 			<Container style={ContainerStyle}>
 				<Timer
 					formatValue={value => `${value < 10 ? `0${value}` : value}`}
-					initialTime={1000 * 60}
+					initialTime={1000 * 3}
 					direction="backward"
 					startImmediately={false}
 					onStart={() => {console.log('onStart hook')}}
@@ -72,28 +101,62 @@ class TimerView extends Component {
 							time: 0,
 							callback: () => {
 								alert('Checkpoint A');
-								this.onResetFunc(this.reset);
+								console.log(this.timer);
+								//this.resetAction();
 
 							},
 						},
 					]}
 				>
-					{({start, resume, pause, stop, reset, timerState}) => {
-						console.log(timerState);
+					{({start, resume, pause, stop, reset, timerState, getTimerState, getTime}) => {
+
+						console.log("getTimerState()", getTimerState());
+
+						//console.log("getTime()", getTime());
+						if(getTime() < 0) {
+
+						}
+
+						console.log(this.props);
+
 						return (
 							<Fragment>
 								<div style={TimerStyle} onClick={() => {
-									console.log(timerState);
-									start();
-									console.log(start);
+
+									/*
+										export const INITED = 'INITED';
+										export const PLAYING = 'PLAYING';
+										export const PAUSED = 'PAUSED';
+										export const STOPPED = 'STOPPED';
+									*/
+									const nowState = getTimerState();
+									const run = nowState === 'INITED' || nowState === 'STOPPED' ? true : false;
+
+									console.log(nowState);
+									console.log(run);
+
+									if(run) {
+										start();
+									} else {
+										stop();
+									}
 								}
+
+								
 								}>
 									{/* <Timer.Days /> days */}
 									<Timer.Hours/>:{/*hours*/}
 									<Timer.Minutes/>:{/*minutes*/}
 									<Timer.Seconds/>{/*seconds*/}
 									{/* <Timer.Milliseconds /> milliseconds */}
+									{/*{this.test(reset, getTime)}*/}
+
+
 								</div>
+								<div style={ResetStyle} onClick={()=> { stop(); reset(); } }  ref={this.resetAction}>
+									<MdRefresh size={"1.5rem"}></MdRefresh><span>Reset</span>
+								</div>
+
 								{/*<div>{timerState}</div>*/}
 								{/*<br/>*/}
 								{/*<div>*/}
